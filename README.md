9854a729b940f848dd78814f68e43b60da553d0a
Aron <45713863+fukuimaunicorn@users.noreply.github.com>
2023-03-20 18:49:05 +0100

    move into monorepo

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 <h1 align="center">YouTube-Live-JS</h1>
 
 Hopefully soon an API wrapper for the youtube live api, similar to [twitch-js](https://github.com/twitch-js/twitch-js).
+
+# MOVED TO https://github.com/studioplascode/multiplatform-streaming-interactions
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,15 @@
 import {google} from "googleapis";
+import * as dotenv from 'dotenv';
 
-const oauth2Client = new google.auth.OAuth2({clientId: ""});
+dotenv.config();
 
-google.options({auth: oauth2Client});
+google.options({auth: process.env.GOOGLE_API_TESTING_KEY});
 
-
-
-google.youtube("v3").liveChatMessages.list({liveChatId: "googleapis", part: ["snippet"]});
+google.youtube("v3").liveBroadcasts.list({part: ["snippet"], mine: true}).then(brodcasts => {
+    const live = brodcasts.data.items![0];
+    const chatId = live.snippet?.liveChatId as string;
+    
+    google.youtube("v3").liveChatMessages.list({liveChatId: chatId, part: ["snippet"]}).then(res => {
+        console.log(res);
+    });
+});
